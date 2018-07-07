f57203b669a4504f7e95ddb53fdbbc2788f01183
apple <libo@goobird.com>
2018-07-07 14:31:19 +0800

    initCommit

---
+++ app/src/main/java/com/example/apple/rongcloudim/MyApp.java
@@ -0,0 +1,13 @@
+package com.example.apple.rongcloudim;
+
+import android.app.Application;
+
+public class MyApp extends Application{
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+
+    }
+}
